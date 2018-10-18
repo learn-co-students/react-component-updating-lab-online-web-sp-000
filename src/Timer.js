91c8c7e8d5ba16ae64bef17c5a6a5f0ff38e15de
Tyler Taylor <vikingtyty@gmail.com>
2018-10-18 18:58:52 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -6,19 +6,31 @@ class Timer extends Component {
     super()
     this.timer = React.createRef()
     this.state = {
-        time: 0,
-        color: '#'+Math.floor(Math.random()*16777215).toString(16),
+      time: 0,
+      color: '#' + Math.floor(Math.random() * 16777215).toString(16),
     }
   }
 
 
 
   //Your code here
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
+  componentDidUpdate() {
+
+  }
+
+
 
 
 
   componentDidMount() {
-    this.interval = setInterval(this.clockTick, this.props.updateInterval*1000)
+    this.interval = setInterval(this.clockTick, this.props.updateInterval * 1000)
   }
 
   componentWillUnmount() {
@@ -28,12 +40,12 @@ class Timer extends Component {
   render() {
     const { time, color, className, logText } = this.state
     return (
-      <section className="Timer" style={{background: color}} ref={this.timer}>
+      <section className="Timer" style={{ background: color }} ref={this.timer}>
 
-        <h1>{ time }</h1>
-        <button onClick={ this.stopClock }>Stop</button>
-        <aside className="logText">{ logText }</aside>
-        <small onClick={ this.handleClose }>X</small>
+        <h1>{time}</h1>
+        <button onClick={this.stopClock}>Stop</button>
+        <aside className="logText">{logText}</aside>
+        <small onClick={this.handleClose}>X</small>
 
       </section>
     );
@@ -48,7 +60,7 @@ class Timer extends Component {
 
   stopClock = () => {
     clearInterval(this.interval)
-    this.setState({className: "hidden"})
+    this.setState({ className: "hidden" })
   }
 
   // for the 'x' button,
