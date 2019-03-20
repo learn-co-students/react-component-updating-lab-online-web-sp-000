2070308e21a6c81e76a3a011a21e4897fa7601d8
Serven Maraghi <maraghiserven@gmail.com>
2019-03-20 11:20:32 -0400

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Timer extends Component {
+class Timer extends PureComponent {
 
   constructor() {
     super()
@@ -16,12 +16,12 @@ class Timer extends Component {
       "#" + Math.floor(Math.random() * 16777215).toString(16)
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if(this.state.time === nextState.time){
-      return false
-    }
-    return true
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if(this.state.time === nextState.time){
+  //     return false
+  //   }
+  //   return true
+  // }
 
 
   componentDidMount() {
