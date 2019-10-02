f13cfc7cb1d59984fd5b02e6a72dc3df98dd4e00
Diana Badas <diana.badas@gmail.com>
2019-10-02 23:41:37 +0100

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -22,6 +22,17 @@ class Timer extends Component {
   componentWillUnmount() {
     clearInterval(this.interval);
   }
+  //I can use current because of the ref attribute (ref={this.timer}) in the section below
+  componentDidUpdate () {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 
   render() {
     const { time, color, logText } = this.state;
