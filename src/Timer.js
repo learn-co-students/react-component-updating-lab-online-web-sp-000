ce03223fdac02f4109577dc0a8114bdcba660d2a
milenaonmaui <milenakari@yahoo.com>
2019-11-06 12:15:11 -0800

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -11,7 +11,19 @@ class Timer extends Component {
   }
 
   //Your code here
+  componentDidUpdate() {
+    //change timer color randomly every time component updates
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+  
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
