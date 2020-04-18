671ac9f4b6e97996880cfa7a0c8d037338898ef8
BlythePollard <github email address>
2020-04-18 14:08:12 -0600

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -19,8 +19,20 @@ class Timer extends Component {
     );
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
   render() {
