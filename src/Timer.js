8ec91eed9838ee5d479fd7c99b4f63b2d75b476a
zdsmit <zdsmith001@gmail.com>
2020-08-04 14:17:24 -0700

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -23,6 +23,18 @@ class Timer extends Component {
     clearInterval(this.interval);
   }
 
+  componentDidUpdate() {
+    this.timer.current.style.color =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
   render() {
     const { time, color, logText } = this.state;
     return (
