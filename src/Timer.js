bc35075a24258dc05ecb5a7d74d827e0e32f38b5
github username <github email address>
2020-08-15 14:25:11 -0700

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -12,6 +12,13 @@ class Timer extends Component {
 
   //Your code here
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false 
+    }
+    return true
+  }
+
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
@@ -21,6 +28,10 @@ class Timer extends Component {
 
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
   render() {
