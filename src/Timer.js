d85de239d122716f3ef63aab7b82dbeb8addace0
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-03-01 16:20:04 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -15,12 +15,12 @@ class Timer extends Component {
     this.timer.current.style.color =
     "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
-  // shouldComponentUpdate(nextProps, nextState){
-  //   if (this.state.time === nextState.time){
-  //     return false
-  //   }
-  //   return true
-  // }
+  shouldComponentUpdate(nextProps, nextState){
+    if (this.state.time === nextState.time){
+      return false
+    }
+    return true
+  }
 
   componentDidMount() {
     this.interval = setInterval(
